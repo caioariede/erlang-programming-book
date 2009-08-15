e261cb5773af1ba17d236d333e8afa0a0e7f38c9
Caio Ariede <caio.ariede@gmail.com>
2009-08-15 11:32:49 -0300

    chapter 3, exercise 1

---
+++ Chapter3/Exercise3-1/sum.erl
@@ -0,0 +1,15 @@
+-module(sum).
+-export([sum/1,sum/2]).
+
+sum(0) ->
+    0;
+
+sum(N) when N > 0 ->
+    N + sum(N - 1).
+
+sum(N, M) when N == M ->
+    N;
+
+sum(N, M) when N =< M ->
+    N + sum(N + 1, M).
+
