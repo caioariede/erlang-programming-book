b4aeff27b02960a5ccccc08987ce8c7e796aba80
Caio Ariede <caio.ariede@gmail.com>
2009-08-15 11:01:23 -0300

    removed all variables from boolean.erl (exercise 2-3)

--- Chapter2/Exercise2-3/boolean.erl
+++ Chapter2/Exercise2-3/boolean.erl
@@ -4,14 +4,17 @@
 b_not(false) ->
     true;
 
-b_not(_Value) ->
+b_not(true) ->
     false.
+
+b_and(true, false) ->
+    false;
+
+b_and(false, true) ->
+    false;
 
 b_and(true, true) ->
-    true;
-
-b_and(_Value1, _Value2) ->
-    false.
+    true.
 
 b_or(true, true) ->
     true;
@@ -22,8 +25,17 @@ b_or(true, false) ->
 b_or(false, true) ->
     true;
 
-b_or(_Value1, _Value2) ->
+b_or(false, false) ->
     false.
 
-b_nand(Value1, Value2) ->
-    boolean:b_and(Value1, Value2) == false.
+b_nand(true, true) ->
+    false;
+
+b_nand(false, true) ->
+    true;
+
+b_nand(true, false) ->
+    true;
+
+b_nand(false, false) ->
+    true.
